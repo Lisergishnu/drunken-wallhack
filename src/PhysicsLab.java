7d674189635f460322293f979116e2df958663ad
Marco Benzi <marco.benzi@alumnos.usm.cl>
2014-06-19 15:09:37 -0400

    Agregando Menus

--- src/PhysicsLab.java
+++ src/PhysicsLab.java
@@ -18,6 +18,8 @@ public class PhysicsLab extends JApplet {
 
    public void init() {
       MyWorldView  worldView = new MyWorldView(world);
+      LabMenuListener menuListener = new LabMenuListener(world);
+      setJMenuBar(createLabMenuBar(menuListener));
       world.setView(worldView);
       getContentPane().add(worldView);
       //Cargar sonido
@@ -40,9 +42,56 @@ public class PhysicsLab extends JApplet {
       
    }
 
+   public JMenuBar createLabMenuBar(LabMenuListener menu_l) {
+	      JMenuBar mb = new JMenuBar();
+	      
+	      JMenu menu = new JMenu ("Configuration");
+	      mb.add(menu);
+	      JMenu subMenu = new JMenu("Insert");  
+	      menu.add(subMenu);
+	      
+	      JMenuItem menuItem = new JMenuItem("Ball");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      menuItem = new JMenuItem("Spring");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      menuItem = new JMenuItem("Fixed Hook");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      menuItem = new JMenuItem("Block");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      menuItem = new JMenuItem("My scenario");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      
+	      menu = new JMenu("MyWorld");
+	      mb.add(menu);
+	      menuItem = new JMenuItem("Start");
+	      menuItem.addActionListener(menu_l);
+	      menu.add(menuItem);
+	      menuItem = new JMenuItem("Stop");
+	      menuItem.addActionListener(menu_l);
+	      menu.add(menuItem);
+	      //Submenu Simulator
+	      subMenu = new JMenu("Simulator");
+	      menu.add(subMenu);
+	      menuItem = new JMenuItem("Delta time");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+	      menuItem = new JMenuItem("View Refresh time");
+	      menuItem.addActionListener(menu_l);
+	      subMenu.add(menuItem);
+
+	      return mb;          
+	   }   
+   
    public static void main(String[] args) {
       PhysicsLab yo = new PhysicsLab();
       PhysicsLab_GUI lab_gui = new PhysicsLab_GUI(yo.getWorld());
+      LabMenuListener menuListener = new LabMenuListener(yo.getWorld());
+      lab_gui.setJMenuBar(yo.createLabMenuBar(menuListener));
       lab_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       lab_gui.setVisible(true);
    }
@@ -52,8 +101,6 @@ class PhysicsLab_GUI extends JFrame {
    public PhysicsLab_GUI(MyWorld world) {
       setTitle("My Small and Nice Physics Laboratory");
       setSize(MyWorldView.WIDTH+100, MyWorldView.HEIGHT+150);  // height+50 to account for menu height
-      LabMenuListener menuListener = new LabMenuListener(world);
-      setJMenuBar(createLabMenuBar(menuListener));
       MyWorldView  worldView = new MyWorldView(world);
       world.setView(worldView);
       getContentPane().add(worldView);
@@ -76,48 +123,5 @@ class PhysicsLab_GUI extends JFrame {
 	}
    }
 
-   public JMenuBar createLabMenuBar(LabMenuListener menu_l) {
-      JMenuBar mb = new JMenuBar();
-      
-      JMenu menu = new JMenu ("Configuration");
-      mb.add(menu);
-      JMenu subMenu = new JMenu("Insert");  
-      menu.add(subMenu);
-      
-      JMenuItem menuItem = new JMenuItem("Ball");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      menuItem = new JMenuItem("Spring");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      menuItem = new JMenuItem("Fixed Hook");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      menuItem = new JMenuItem("Block");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      menuItem = new JMenuItem("My scenario");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      
-      menu = new JMenu("MyWorld");
-      mb.add(menu);
-      menuItem = new JMenuItem("Start");
-      menuItem.addActionListener(menu_l);
-      menu.add(menuItem);
-      menuItem = new JMenuItem("Stop");
-      menuItem.addActionListener(menu_l);
-      menu.add(menuItem);
-      //Submenu Simulator
-      subMenu = new JMenu("Simulator");
-      menu.add(subMenu);
-      menuItem = new JMenuItem("Delta time");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-      menuItem = new JMenuItem("View Refresh time");
-      menuItem.addActionListener(menu_l);
-      subMenu.add(menuItem);
-
-      return mb;          
-   }   
+   
 }
