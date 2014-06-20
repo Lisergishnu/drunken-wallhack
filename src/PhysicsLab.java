28386e68364094ad21d476a65c910646df090543
Marco Benzi <marco.benzi@alumnos.usm.cl>
2014-06-19 20:44:34 -0400

    Applet lista con división

--- src/PhysicsLab.java
+++ src/PhysicsLab.java
@@ -1,168 +1,112 @@
 import javax.swing.JFrame;
+import javax.swing.JSplitPane;
 import javax.sound.sampled.*;
 import javax.swing.*;
+
+import org.math.plot.Plot2DPanel;
 
 import java.awt.Container;
 import java.io.IOException;
 import java.net.URL;
 
-//OBS: Esta clase hace la funcion de la clase PhysicsLab Y PhysicsLabApplet, segun la tarea
-public class PhysicsLab extends JApplet {
+public class PhysicsLab {
 
-   private MyWorld world;
-   public MyWorld getWorld() {return world;}
+	private MyWorld world;
+	public MyWorld getWorld() {return world;}
 
-   public PhysicsLab() {
-      world = new MyWorld();
-   }
-
-   public void init() {
-      MyWorldView  worldView = new MyWorldView(world);
-      LabMenuListener menuListener = new LabMenuListener(world);
-      setJMenuBar(createLabMenuBar(menuListener));
-      world.setView(worldView);
-      getContentPane().add(worldView);
-
-      //Cargar sonido
-      URL url = this.getClass().getClassLoader().getResource("collision.wav");
-      try {
-		AudioInputStream audio = AudioSystem.getAudioInputStream(url);
-		Clip clipCollision = AudioSystem.getClip();
-		clipCollision.open(audio);
-		world.setCollisionClip(clipCollision);
-	} catch (UnsupportedAudioFileException e) {
-		System.out.println("Audio file Unsupported.");
-		e.printStackTrace();
-	} catch (IOException e) {
-		System.out.println("Can't open file.");
-		e.printStackTrace();
-	} catch (LineUnavailableException e) {
-		System.out.println("Cannot open audio clip line.");
-		e.printStackTrace();
+	public PhysicsLab() {
+		world = new MyWorld();
 	}
-      
-      //Cargar parametros desde HTML
-      world.setTitle(getParameter("title"));
-      world.setDelta_t(Double.parseDouble(getParameter("deltaTime")));
-      world.setRefreshPeriod(Double.parseDouble(getParameter("refreshTime")));
-      //TODO: Set max plotting time
-      //something something (Double.parseDouble(getParameter("maxPlotTime")))
-      
-      //Ver cuantos objetos hay descritos y agregarlos a MyWorld
-      //Bolas
-      int nBolas = Integer.parseInt(getParameter("ballNum"));
-      for (int i = 0; i < nBolas; i++) {
-		//Obtener parametros
-    	String param = getParameter("ball." + (i+1) );
-    	String[] paramList = param.split(";");
-    	world.addElement(new Ball(Double.parseDouble(paramList[0]),
-    							  Double.parseDouble(paramList[1]),
-    							  Double.parseDouble(paramList[2]),
-    							  Double.parseDouble(paramList[3])));
-      }
-      
-      //FixedHooks
-      int nHooks = Integer.parseInt(getParameter("fixedHookNum"));
-      for (int i = 0; i < nHooks; i++) {
-		//Obtener parametros
-    	String param = getParameter("fixedHook." + (i+1) );
-    	String[] paramList = param.split(";");
-    	world.addElement(new FixedHook(Double.parseDouble(paramList[0]),
-    							  Double.parseDouble(paramList[1])));
-      }
-      
-      //Osciladores
-      int nOsc = Integer.parseInt(getParameter("oscillatorNum"));
-      for (int i = 0; i < nOsc; i++) {
-		//Obtener parametros
-    	String param = getParameter("oscillator." + (i+1) );
-    	String[] paramList = param.split(";");
-    	world.addElement(new FixedHook(Double.parseDouble(paramList[0]),
-    							  Double.parseDouble(paramList[1])));
-      }
-      
-   }
+	
+	/*
+	 * @returns JSplitPane Panel listo para anadir al contexto requerido (Applet o JPanel)
+	 */
+	public JSplitPane initialize() {
+		MyWorldView  worldView = new MyWorldView(world);
+		world.setView(worldView);
+		worldView.setTitle("Running as a standalone app");
+		Plot2DPanel plot = new Plot2DPanel();
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, worldView, plot);
+		//Cargar sonido
+		URL url = this.getClass().getClassLoader().getResource("collision.wav");
+		try {
+			AudioInputStream audio = AudioSystem.getAudioInputStream(url);
+			Clip clipCollision = AudioSystem.getClip();
+			clipCollision.open(audio);
+			world.setCollisionClip(clipCollision);
+		} catch (UnsupportedAudioFileException e) {
+			System.out.println("Audio file Unsupported.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Can't open file.");
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			System.out.println("Cannot open audio clip line.");
+			e.printStackTrace();
+		}
+		
+		return splitPane;
+	}
 
-   public JMenuBar createLabMenuBar(LabMenuListener menu_l) {
-	      JMenuBar mb = new JMenuBar();
-	      
-	      JMenu menu = new JMenu ("Configuration");
-	      mb.add(menu);
-	      JMenu subMenu = new JMenu("Insert");  
-	      menu.add(subMenu);
-	      
-	      JMenuItem menuItem = new JMenuItem("Ball");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      menuItem = new JMenuItem("Spring");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      menuItem = new JMenuItem("Fixed Hook");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      menuItem = new JMenuItem("Block");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      menuItem = new JMenuItem("My scenario");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      
-	      menu = new JMenu("MyWorld");
-	      mb.add(menu);
-	      menuItem = new JMenuItem("Start");
-	      menuItem.addActionListener(menu_l);
-	      menu.add(menuItem);
-	      menuItem = new JMenuItem("Stop");
-	      menuItem.addActionListener(menu_l);
-	      menu.add(menuItem);
-	      //Submenu Simulator
-	      subMenu = new JMenu("Simulator");
-	      menu.add(subMenu);
-	      menuItem = new JMenuItem("Delta time");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
-	      menuItem = new JMenuItem("View Refresh time");
-	      menuItem.addActionListener(menu_l);
-	      subMenu.add(menuItem);
+	public static void main(String[] args) {
+		PhysicsLab yo = new PhysicsLab();
+		PhysicsLab_GUI lab_gui = new PhysicsLab_GUI();
+		LabMenuListener menuListener = new LabMenuListener(yo.getWorld());
+		lab_gui.setJMenuBar(yo.createLabMenuBar(menuListener));
+		lab_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lab_gui.add(yo.initialize());
+		lab_gui.setVisible(true);
+	}
 
-	      return mb;          
-	   }   
-   
-   public static void main(String[] args) {
-      PhysicsLab yo = new PhysicsLab();
-      PhysicsLab_GUI lab_gui = new PhysicsLab_GUI(yo.getWorld());
-      LabMenuListener menuListener = new LabMenuListener(yo.getWorld());
-      lab_gui.setJMenuBar(yo.createLabMenuBar(menuListener));
-      lab_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      lab_gui.setVisible(true);
-   }
+	public JMenuBar createLabMenuBar(LabMenuListener menu_l) {
+		JMenuBar mb = new JMenuBar();
+
+		JMenu menu = new JMenu ("Configuration");
+		mb.add(menu);
+		JMenu subMenu = new JMenu("Insert");  
+		menu.add(subMenu);
+
+		JMenuItem menuItem = new JMenuItem("Ball");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Spring");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Fixed Hook");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Block");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("My scenario");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+
+		menu = new JMenu("MyWorld");
+		mb.add(menu);
+		menuItem = new JMenuItem("Start");
+		menuItem.addActionListener(menu_l);
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Stop");
+		menuItem.addActionListener(menu_l);
+		menu.add(menuItem);
+		//Submenu Simulator
+		subMenu = new JMenu("Simulator");
+		menu.add(subMenu);
+		menuItem = new JMenuItem("Delta time");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("View Refresh time");
+		menuItem.addActionListener(menu_l);
+		subMenu.add(menuItem);
+
+		return mb;          
+	} 
 }
 
-class PhysicsLab_GUI extends JFrame {
-   public PhysicsLab_GUI(MyWorld world) {
-      setTitle("My Small and Nice Physics Laboratory");
-      setSize(MyWorldView.WIDTH+100, MyWorldView.HEIGHT+150);  // height+50 to account for menu height
-      MyWorldView  worldView = new MyWorldView(world);
-      world.setView(worldView);
-      getContentPane().add(worldView);
-      //Cargar sonido
-      URL url = this.getClass().getClassLoader().getResource("collision.wav");
-      try {
-		AudioInputStream audio = AudioSystem.getAudioInputStream(url);
-		Clip clipCollision = AudioSystem.getClip();
-		clipCollision.open(audio);
-		world.setCollisionClip(clipCollision);
-	} catch (UnsupportedAudioFileException e) {
-		System.out.println("Audio file Unsupported.");
-		e.printStackTrace();
-	} catch (IOException e) {
-		System.out.println("Can't open file.");
-		e.printStackTrace();
-	} catch (LineUnavailableException e) {
-		System.out.println("Cannot open audio clip line.");
-		e.printStackTrace();
-	}
-   }
-
-   
+class PhysicsLab_GUI extends JInternalFrame {
+	public PhysicsLab_GUI() {
+		setTitle("My Small and Nice Physics Laboratory");
+		setSize(MyWorldView.WIDTH+100, MyWorldView.HEIGHT+150);  // height+50 to account for menu height
+	}   
 }
---
+++ src/PhysicsLabApplet.java
@@ -0,0 +1,67 @@
+import javax.swing.JApplet;
+import javax.swing.JSplitPane;
+
+
+public class PhysicsLabApplet extends JApplet {
+
+	private PhysicsLab phyLab;
+	private MyWorld world;
+	private JSplitPane splitPane;
+	
+	public PhysicsLabApplet() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void init() {
+		phyLab = new PhysicsLab();
+		world = phyLab.getWorld();
+		splitPane = phyLab.initialize();
+		LabMenuListener menuListener = new LabMenuListener(world);
+		setJMenuBar(phyLab.createLabMenuBar(menuListener));
+		getContentPane().add(splitPane);
+		
+		//Cargar parametros desde HTML
+		world.setTitle(getParameter("title"));
+		world.setDelta_t(Double.parseDouble(getParameter("deltaTime")));
+		world.setRefreshPeriod(Double.parseDouble(getParameter("refreshTime")));
+		//TODO: Set max plotting time
+		//something something (Double.parseDouble(getParameter("maxPlotTime")))
+
+		//Ver cuantos objetos hay descritos y agregarlos a MyWorld
+		//Bolas
+		int nBolas = Integer.parseInt(getParameter("ballNum"));
+		for (int i = 0; i < nBolas; i++) {
+			//Obtener parametros
+			String param = getParameter("ball." + (i+1) );
+			String[] paramList = param.split(";");
+			world.addElement(new Ball(Double.parseDouble(paramList[0]),
+					Double.parseDouble(paramList[1]),
+					Double.parseDouble(paramList[2]),
+					Double.parseDouble(paramList[3])));
+		}
+
+		//FixedHooks
+		int nHooks = Integer.parseInt(getParameter("fixedHookNum"));
+		for (int i = 0; i < nHooks; i++) {
+			//Obtener parametros
+			String param = getParameter("fixedHook." + (i+1) );
+			String[] paramList = param.split(";");
+			world.addElement(new FixedHook(Double.parseDouble(paramList[0]),
+					Double.parseDouble(paramList[1])));
+		}
+
+		//Osciladores
+		int nOsc = Integer.parseInt(getParameter("oscillatorNum"));
+		for (int i = 0; i < nOsc; i++) {
+			//Obtener parametros
+			String param = getParameter("oscillator." + (i+1) );
+			String[] paramList = param.split(";");
+			world.addElement(new FixedHook(Double.parseDouble(paramList[0]),
+					Double.parseDouble(paramList[1])));
+		}
+	}
+	
+	public void start() {
+		splitPane.setDividerLocation(0.75);
+	}
+}
